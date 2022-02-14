fce27cdf3b04e2d74140fb956674cd7aaf5dbfdd
Phong <hongphongle774@gmail.com>
2022-02-14 13:45:48 +0700

    update

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,2 +1,67 @@
-public class GamePanel {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements ActionListener{
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNIT = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 75;
+    final int x[] = new int[GAME_UNIT];
+    final int y[] = new int[GAME_UNIT];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+    GamePanel() {
+
+    }
+    public void StartGame () {
+//        newApple();
+        running=true;
+        timer=new Timer(DELAY,this);
+        timer.start();
+    }
+    public void paintComponent (Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+    public void draw (Graphics g) {
+        for (int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
+            g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
+            g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
+        }
+    }
+    public void newApple() {
+
+    }
+    public void move (){
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollisons(){
+
+    }
+    public void gameover(Graphics g){
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+    }
 }
--- src/GameScreen.java
+++ src/GameScreen.java
@@ -1,3 +1,6 @@
 public class GameScreen {
+    public static void main(String[] args) {
+        new GameFrame();
 
+    }
 }
