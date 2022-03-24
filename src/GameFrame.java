003c12238a44b8d0004e2765c4744c4a23c4cd54
Hiếu Trần <tranhieu291102@gmail.com>
2022-03-24 11:22:16 +0700

    update menu

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -7,6 +7,7 @@ public class GameFrame extends JFrame implements ActionListener{
     JButton btnStart;
     JLabel L1;
     JButton resetButton;
+    JMenuItem About=new JMenuItem("About");
     GameFrame () {
         this.setContentPane(new JLabel(new ImageIcon("sng.jpg")));
 //        this.setLayout(new FlowLayout());
@@ -25,9 +26,28 @@ public class GameFrame extends JFrame implements ActionListener{
         this.btnStart.setText("Start");
         this.btnStart.addActionListener(this);
         this.setResizable(false);
+
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==About){
+            JFrame jf=new JFrame();
+            jf.setSize(500,500);
+            jf.setVisible(true);
+            jf.setTitle("About");
+            jf.setLayout(null);
+            jf.setLocationRelativeTo(null);
+//            jf.setContentPane(new JLabel("Tran Trung Hieu-2012101208Le Hong Phong-201200271"));
+//            JLabel j1=new JLabel("Tran Trung Hieu-2012101208");
+//            jf.add(j1);
+            JLabel label=new JLabel();
+            label.setText("Tran Trung Hieu");
+            label.setAlignmentX(0);
+            label.setAlignmentY(0);
+            label.setForeground(Color.red);
+            jf.add(label);
+            
+        }
         if (e.getSource()==btnStart) {
             this.setVisible(false);
             JFrame jFrame = new JFrame();
@@ -38,6 +58,21 @@ public class GameFrame extends JFrame implements ActionListener{
             jFrame.pack();
             jFrame.setVisible(true);
             jFrame.setLocationRelativeTo(null);
+
+            JMenuBar menuBar=new JMenuBar();
+            JMenu fileMenu =new JMenu("File");
+            JMenu HelpMenu =new JMenu("Help");
+            JMenu LeaderBoardMenu =new JMenu("LeaderBoard");
+
+
+            About.addActionListener(this);
+
+            HelpMenu.add(About);
+            menuBar.add(fileMenu);
+            menuBar.add(HelpMenu);
+            menuBar.add(LeaderBoardMenu);
+            jFrame.setJMenuBar(menuBar);
+
         }
     }
 }
