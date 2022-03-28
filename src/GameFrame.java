299c8af9e6a2fda549fb2fbaefc0be38689f2110
Hiếu Trần <tranhieu291102@gmail.com>
2022-03-28 10:52:06 +0700

    update backgorund about

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -33,6 +33,7 @@ public class GameFrame extends JFrame implements ActionListener{
         JMenu HelpMenu = new JMenu("Help");
         JMenu LeaderBoardMenu = new JMenu("LeaderBoard");
         HelpMenu.add(About);
+
         About.addActionListener(this);
         LeaderBoardMenu.add(ShowLeaderBoard);
         ShowLeaderBoard.addActionListener(this);
@@ -78,7 +79,7 @@ public class GameFrame extends JFrame implements ActionListener{
         }
         if (e.getSource()==ShowLeaderBoard) {
             String []data = new String[100];
-            var sql = "select * from playersnakegame";
+            var sql = "select * from playersnakegame ORDER BY Diem DESC";
             try (Connection conn = DriverManager.getConnection(url,user,password)) {
                 Statement statement = conn.createStatement();
                 ResultSet resultSet = statement.executeQuery(sql);
@@ -107,6 +108,7 @@ public class GameFrame extends JFrame implements ActionListener{
         }
         if(e.getSource()==About){
             JFrame jf=new JFrame();
+            jf.setContentPane(new JLabel(new ImageIcon("SnakeGameAboutbackground.jpg")));
             jf.setSize(1000,600);
             jf.setVisible(true);
             jf.setTitle("About");
@@ -118,20 +120,24 @@ public class GameFrame extends JFrame implements ActionListener{
             jLabel1.setText("Thành viên trong nhóm");
             jLabel1.setBounds(120,50,500,30);
             jLabel1.setFont(new Font("Serif",Font.PLAIN,20));
+            jLabel1.setForeground(Color.yellow);
             jf.add(jLabel1);
             JLabel jLabel2 = new JLabel();
             jLabel2.setText("Trần Trung Hiếu - 201210128");
             jLabel2.setBounds(120,75,500,30);
             jLabel2.setFont(new Font("Serif",Font.PLAIN,20));
+            jLabel2.setForeground(Color.yellow);
             jf.add(jLabel2);
             JLabel jLabel3 = new JLabel();
             jLabel3.setText("Lê Hồng Phong - 201200271");
             jLabel3.setBounds(120,100,500,30);
             jLabel3.setFont(new Font("Serif",Font.PLAIN,20));
+            jLabel3.setForeground(Color.yellow);
             jf.add(jLabel3);
-            JLabel jLabel = new JLabel("<html><h3 style='color:black'>Thông tin về game</h1><p>Qua nghiên cứu và tìm hiểu ta nhận thấy game Rắn săn mồi là game cổ điện xuất hiện vào năm 1997 trên Nokia 6610 là những ô vuông xếp liên tiếp nhau di chuyển trên một màn hình màu xanh đơn giản, nhưng Rắn săn mồi đã xây dựng rất thành công tên tuổi của mình. Với 400 triệu bản được xuất xưởng và đến hiện tại đã là phiên bản thứ 8. Game có sự hấp đẫn đối với người chơi. Do đó nhóm em quyết định xây dựng game dựa trên những ý tưởng từ xưa cũ nhưng chưa bao giờ là mất đi chất của tựa game dã tạo nên tuổi thơ của một thế hệ. ");
+            JLabel jLabel = new JLabel("<html><h3 style='color:yellow'>Thông tin về game</h1><p>Qua nghiên cứu và tìm hiểu ta nhận thấy game Rắn săn mồi là game cổ điện xuất hiện vào năm 1997 trên Nokia 6610 là những ô vuông xếp liên tiếp nhau di chuyển trên một màn hình màu xanh đơn giản, nhưng Rắn săn mồi đã xây dựng rất thành công tên tuổi của mình. Với 400 triệu bản được xuất xưởng và đến hiện tại đã là phiên bản thứ 8. Game có sự hấp đẫn đối với người chơi. Do đó nhóm em quyết định xây dựng game dựa trên những ý tưởng từ xưa cũ nhưng chưa bao giờ là mất đi chất của tựa game dã tạo nên tuổi thơ của một thế hệ. ");
             jLabel.setBounds(120,115,500,400);
             jLabel.setFont(new Font("Serif",Font.PLAIN,20));
+            jLabel.setForeground(Color.red);
             jf.add(jLabel);
         }
     }
